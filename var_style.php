28f6ded473caefccacc5b18ec94dc4e4bd95249d
netinvman <linux-fox#163.com>
2015-08-19 14:42:57 +0800

    add var_style.php

---
+++ var_style.php
@@ -0,0 +1,28 @@
+<?php
+$a = "";
+$b = 3.1;
+$c = True;
+$d = 2147483647;
+$e = 2147483648;
+$f = 0x80000000;
+$g = "hi";
+$h = array(1, 2);
+
+echo '$a: ';
+var_dump($a);
+echo '<br>$b: ';
+var_dump($b);
+echo '<br>$c: ';
+var_dump($c);
+echo '<br>$d: ';
+var_dump($d);
+echo '<br>$e: ';
+var_dump($e);
+echo '<br>$f: ';
+var_dump($f);
+echo '<br>$g: ';
+var_dump($g);
+echo '<br>$h: ';
+var_dump($h);
+
+?>
