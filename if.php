c69f8c40e6b06a1d895b03d8fbf9f58905162d6d
netinvman <linux-fox#163.com>
2015-08-21 15:20:53 +0800

    add if.php

---
+++ if.php
@@ -0,0 +1,14 @@
+<?php
+$achievement = 91;
+if ($achievement < 60)
+    echo "so bad";
+elseif ($achievement >= 60 && $achievement < 70) 
+    echo "good";
+elseif ( $achievement >= 70 and $achievement < 80)
+    echo "better";
+elseif ($achievement >= 80 and $achievement < 90)
+    echo "better, better!";
+else
+    echo "you are so best";
+
+?>
