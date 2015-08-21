cbf02f2ad17e133122d688265bbe48c408c9438a
netinvman <linux-fox#163.com>
2015-08-21 10:03:11 +0800

    add arithmetic.php

---
+++ arithmetic.php
@@ -0,0 +1,10 @@
+<?php
+$ONE = 1;
+$TWO = 2;
+$THREE = 3;
+echo $ONE + $TWO . "<br>";
+echo $ONE - $TWO . "<br>";
+echo $ONE * $TWO . "<br>";
+echo $ONE / $TWO . "<br>";
+echo $THREE % $TWO . "<br>";
+?>
