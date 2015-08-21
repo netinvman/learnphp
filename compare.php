40cd4e5cc76ba7dfbe7fb9dbd5d515729b64b0b1
netinvman <linux-fox#163.com>
2015-08-21 14:35:07 +0800

    add compare.php

---
+++ compare.php
@@ -0,0 +1,8 @@
+<?php
+echo "10 less than 10? " . ((10 < 10)?"True":"False") . "<br>";
+echo "10 less qual than 10? " . ((10 <= 10)?"True":"False") . "<br>";
+echo "10 greater than 10? " . ((10 > 10)?"True":"False") . "<br>";
+echo "10 greater equal than 10?" . ((10 >= 10)?"True":"False") . "<br>";
+echo "10 equal 10?" . ((10 == 10)?"True":"False") . "<br>";
+echo "10 not equal 10?" . ((10 != 10)?"True":"False") . "<br>";
+?>
