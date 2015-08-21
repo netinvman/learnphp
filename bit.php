272f6230d610535e1bb34b8f5c1a2f9e074d4679
netinvman <linux-fox#163.com>
2015-08-21 11:07:51 +0800

    add bit.php

---
+++ bit.php
@@ -0,0 +1,12 @@
+<?php
+$a = 7;
+$b = 14;
+$myVal = $a & $b;
+echo $myVal . "<br>";
+$myVal = $a | $b;
+echo $myVal . "<br>";
+$myVal = $a ^ $b;
+echo $myVal . "<br>";
+$myVal = ~$a;
+echo $myVal . "<br>";
+?>
