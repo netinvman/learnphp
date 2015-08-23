45321782b6c0d5f57b981fed7ba04e66e0ee76e1
netinvman <linux-fox@163.com>
2015-08-23 11:19:34 +0800

    add multi_array.php

---
+++ multi_array.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?php
+$student = array("0"=>array("name"=>"James", "sex"=>"male", "age"=>"28"), "1"=>array("name"=>"John", "sex"=>"male", "age"=>"25"), "2"=>array("name"=>"Susan", "sex"=>"female", "age"=>"24"));
+print $student[2]["age"];
+?>
+</body>
+</html>
