71fe67e1f54c0fca524eace3a72d44cba98730bb
netinvman <linux-fox#163.com>
2015-08-25 16:24:08 +0800

    add print.php

---
+++ print.php
@@ -0,0 +1,21 @@
+<?php
+print("Hello World");
+print "<br>";
+
+print "i
+want to fly high";
+print "<br>";
+
+print "\"i am here\".";
+$foo = "foobar";
+$bar = "barbaz";
+print "<br>";
+
+print "foo is $foo";
+print "<br>";
+
+print <<<END
+This uses the "here document" syntax to output
+multiple line with $variable interpolation.
+END
+?>
