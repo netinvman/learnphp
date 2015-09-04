56eb99bc813d18f03dde4fd552a02f95ec24e451
netinvman <linux-fox@163.com>
2015-09-04 21:01:52 +0800

    add preg_match_2.php

---
+++ preg_match_2.php
@@ -0,0 +1,13 @@
+<?php
+if (preg_match("/\bweb\b/i", "PHP is web scripting language.")) {
+    print "find match.<br>";
+} else {
+    print "not find match.<br>";
+}
+
+if (preg_match("/\bweb\b/i", "PHP is the website scripting language.") {
+    print 'find match';
+} else {
+    print 'not find match.';
+}
+?>
