d3b10c56facc7b0ff549b5c9c843f7125d16152a
netinvman <linux-fox#163.com>
2015-09-14 10:41:43 +0800

    add firstclass.php

---
+++ firstclass.php
@@ -0,0 +1,12 @@
+<?php
+class myName
+{
+    function __construct($myName)
+    {
+        echo "my name is: $myName<br>";
+    }
+}
+$name1 = new myName("dog");
+$name2 = new myName("cat");
+$name3 = new myName("horse");
+?>
