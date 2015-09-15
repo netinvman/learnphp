998f7ae2b3f630389e229d5e2119324595a14810
netinvman <linux-fox@163.com>
2015-09-15 15:19:40 +0800

    add class_1.php

--- class_1.php
+++ class_1.php
@@ -1,14 +1,18 @@
 <?php
-class Cart
-{
-    var $items;
-    function add_item($artnr, $num)
-    {
-        pass;
-    }
-    function remove_item($artnr, $num)
-    {
-        pass;
+class A {
+    function example() {
+        echo "i am base class A::exmple().<br>";
     }
 }
+
+class B {
+    function example() {
+        echo "i am child class 's function B::example().<br>"
+	A::example();
+    }
+}
+
+A::example();
+$b = new B:
+$b->example();
 ?>
